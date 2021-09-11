4dd93a7c72c539b906c25fae41af7abca0218d96
Diogo Cerqueira <diogosantoscerqueira@gmail.com>
2021-09-11 18:22:15 -0300

    Hash fixado

--- app/Http/Controllers/Usuario.php
+++ app/Http/Controllers/Usuario.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Hash;
 
 class Usuario extends Controller
 {
     public function cadastrar(){
-        //
+        return view('usuario.cadastro');
     }
 
     public function salvar(Request $request){
