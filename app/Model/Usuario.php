720ac0b7dc90bb7466a12813db265e88aaa78c7b
Diogo Cerqueira <diogosantoscerqueira@gmail.com>
2021-09-11 16:49:09 -0300

    Usuario Model add

---
+++ app/Model/Usuario.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model
+{
+    protected $connection = 'sqlite';
+    protected $table = 'usuario';
+
+    public static function listar(int $limite){
+        $sql = self::select([
+            "id",
+            "nome",
+            "email",
+            "data_cadastro"
+        ])->limit($limite)->get();
+
+        dd($sql->toSql());
+    }
+
+    public static function cadastrar(Request $request){
+        $sql = self::insert([
+            "nome" => $request->input('nome'),
+            "email" => $request->input('email'),
+            "data_cadastro" => DB::raw('NOW()'),
+        ]);
+
+        dd($sql->toSql(), $request->all());
+    }
+}
